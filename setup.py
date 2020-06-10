8b8c2f562bfdda4a47b410e0ead28c53b38d0e49
Tim-Luca Lagmöller <lagmoellertim@gmail.com>
2020-06-10 21:53:35 +0200

    Updated to next version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='unsilence',
-    version='1.0.1',
+    version='1.0.2',
     install_requires=requirements,
     license='MIT License',
     author='Tim-Luca Lagmöller',
