8e895ca4625304d99e952d2b9a92a7898b119b10
Tim-Luca Lagmöller <lagmoellertim@gmail.com>
2020-07-14 18:13:13 +0200

    Increased Version Numberr

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='unsilence',
-    version='1.0.3',
+    version='1.0.4',
     install_requires=requirements,
     license='MIT License',
     author='Tim-Luca Lagmöller',
